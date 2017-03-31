1c6c57a15d635e7b98c8af01d99938a53c9b0795
yi <liangdahong@bluemoon.com.cn>
2017-04-01 06:11:36 +0800

    .

--- RuntimeTest/RuntimeTest/UIButton+Rect.m
+++ RuntimeTest/RuntimeTest/UIButton+Rect.m
@@ -11,6 +11,9 @@
 
 @implementation UIButton (Rect)
 
+
+
+
 - (void)setImageRect:(CGRect)imageRect
 {
     objc_setAssociatedObject(self, @selector(imageRect), [NSValue valueWithCGRect:imageRect] , OBJC_ASSOCIATION_RETAIN_NONATOMIC);
@@ -52,16 +55,21 @@ void swizzleMethod(Class class, SEL originalSelector, SEL swizzledSelector)
 
 + (void)load
 {
+    //load 时 已经交换了方法imageRectForContentRect 以及 zy_imageRectForContentRect
+    //这时系统调用 imageRectForContentRect 方法，其实是调用了 zy_imageRectForContentRect 方法
+    //相反，调用 zy_imageRectForContentRect 方法，其实是调用了 imageRectForContentRect 方法
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         swizzleMethod([self class], @selector(imageRectForContentRect:), @selector(zy_imageRectForContentRect:));
         swizzleMethod([self class], @selector(titleRectForContentRect:), @selector(zy_titleRectForContentRect:));
     });
 }
+
 - ( CGRect )zy_imageRectForContentRect:( CGRect )contentRect
 {
-    //设置了值就返回设置的值,否则用系统的
-    
+    //系统会自动调用imageRectForContentRect方法，这时候方法已经被交换，其实调用的是重写后的 zy_imageRectForContentRect 方法。
+    // 如果用户设置了 self.imageRect ，即self.imageRect 不等于 CGRectZero，这时直接返回用户设置的 imageRect
+    // 如果self.imageRect 等于 CGRectZero，用户没有设置 imageRect 属性值，这时调用 zy_imageRectForContentRect 方法，因为 imageRectForContentRect 与 zy_imageRectForContentRect 做了交换，这时调用的是 imageRectForContentRect，保证用户没设置imageRect时，是原先button的样式
     if (!CGRectEqualToRect(self.imageRect,CGRectZero)) {
         return self.imageRect;
     }
