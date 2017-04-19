72c22593cf6d3e702e106f043ffac419fc9afd59
yi <liangdahong@bluemoon.com.cn>
2017-04-20 07:40:03 +0800

    .

---
+++ XXNibBridge-master/Demo/XXNibBridgeDemo/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  XXNibBridgeDemo
+//
+//  Created by __liangdahong on 2017/4/20.
+//  Copyright © 2017年 sunnyxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Test : UIView
+
+@end
---
+++ XXNibBridge-master/Demo/XXNibBridgeDemo/Test.m
@@ -0,0 +1,26 @@
+//
+//  Test.m
+//  XXNibBridgeDemo
+//
+//  Created by __liangdahong on 2017/4/20.
+//  Copyright © 2017年 sunnyxx. All rights reserved.
+//
+
+#import "Test.h"
+#import "XXNibBridge.h"
+
+@interface Test ()<XXNibBridge>
+
+@end
+
+@implementation Test
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
